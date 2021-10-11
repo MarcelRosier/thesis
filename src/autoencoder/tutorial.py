3234cccccb466a1d2a29b2556dfb93683ce5f4b3
Marcel Rosier <marcel.rosier@tum.de>
2021-10-11 15:02:58 +0200

    - added tutorial code for a basic autoencoder as reference

---
+++ src/autoencoder/tutorial.py
@@ -0,0 +1,154 @@
+from torch import nn
+from torch import optim
+from torch.functional import Tensor
+import torchvision
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class AE(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.encoder_hidden_layer = nn.Linear(
+            in_features=kwargs["input_shape"], out_features=128
+        )
+        self.encoder_output_layer = nn.Linear(
+            in_features=128, out_features=128
+        )
+        self.decoder_hidden_layer = nn.Linear(
+            in_features=128, out_features=128
+        )
+        self.decoder_output_layer = nn.Linear(
+            in_features=128, out_features=kwargs["input_shape"]
+        )
+
+    def forward(self, features):
+        activation = self.encoder_hidden_layer(features)
+        activation = torch.relu(activation)
+        code = self.encoder_output_layer(activation)
+        code = torch.relu(code)
+        activation = self.decoder_hidden_layer(code)
+        activation = torch.relu(activation)
+        activation = self.decoder_output_layer(activation)
+        reconstructed = torch.relu(activation)
+        return reconstructed
+
+
+#  use gpu if available
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
+# create a model from `AE` autoencoder class
+# load it to the specified device, either gpu or cpu
+model = AE(input_shape=784).to(device)
+
+# create an optimizer object
+# Adam optimizer with learning rate 1e-3
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+# mean-squared error loss
+criterion = nn.MSELoss()
+
+
+###
+# Get and load dataset
+###
+transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+
+train_dataset = torchvision.datasets.MNIST(
+    root="~/Projects/torch_datasets", train=True, transform=transform, download=True
+)
+
+# test_dataset = torchvision.datasets.MNIST(
+#     root="~/Projects/torch_datasets", train=False, transform=transform, download=True
+# )
+
+train_loader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=128, shuffle=True, num_workers=4, pin_memory=True
+)
+
+# test_loader = torch.utils.data.DataLoader(
+#     test_dataset, batch_size=32, shuffle=False, num_workers=4
+# )
+
+###
+# Run training
+###
+
+
+epochs = 20
+
+for epoch in range(epochs):
+    loss = 0
+    for batch_features, _ in train_loader:
+        # reshape mini-batch data to [N, 784] matrix
+        # load it to the active device
+        # ?-1 means that this parameter will be inferred to to match the original size
+        batch_features = batch_features.view(-1, 784).to(device)
+
+        # reset the gradients back to zero
+        # PyTorch accumulates gradients on subsequent backward passes
+        optimizer.zero_grad()
+
+        # compute reconstructions = x_hat
+        outputs = model(batch_features)
+
+        # compute training reconstruction loss (MSELoss = MeanSquaredErrorLoss)
+        # compare x_hat with x
+        train_loss = criterion(outputs, batch_features)
+
+        # compute accumulated gradients
+        # perform backpropagation of errors
+        train_loss.backward()
+
+        # perform parameter update based on current gradients
+        # optimize weights
+        optimizer.step()
+
+        # add the mini-batch training loss to epoch loss
+        loss += train_loss.item()
+
+    # compute the epoch training loss
+    loss = loss / len(train_loader)
+
+    # display the epoch training loss
+    print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, epochs, loss))
+
+print("Performing and visualizing test data")
+
+test_dataset = torchvision.datasets.MNIST(
+    root="~/torch_datasets", train=False, transform=transform, download=True
+)
+
+test_loader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=10, shuffle=False
+)
+
+test_examples = None
+
+with torch.no_grad():
+    for batch_features in test_loader:
+        batch_features = batch_features[0]
+        test_examples = batch_features.view(-1, 784)
+        reconstruction = model(test_examples)
+        break
+
+
+with torch.no_grad():
+    number = 10
+    plt.figure(figsize=(20, 4))
+    for index in range(number):
+        # display original
+        ax = plt.subplot(2, number, index + 1)
+        plt.imshow(test_examples[index].numpy().reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # display reconstruction
+        ax = plt.subplot(2, number, index + 1 + number)
+        plt.imshow(reconstruction[index].numpy().reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
