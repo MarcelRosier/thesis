8171a34aa8e3c38ccf57bd3e0a79687c233c6967
Marcel Rosier <marcel.rosier@gmx.de>
2022-01-19 12:47:39 +0100

    - added vae train val loss data

--- src/tensorboard_parser.py
+++ src/tensorboard_parser.py
@@ -68,29 +68,34 @@ def save_train_loss_as_pkl(exp_name):
     # print(log_df.loc[0].step)
     print(log_df)
 
-    for i in range(0, 600, 2):  # for older 600,5
+    for i in range(0, 1800, 3):  # for older 600,5
         train_loss = log_df.loc[i].value
         val_loss = log_df.loc[i+1].value
-        # kld_loss = log_df.loc[i+2].value
+        kld_loss = log_df.loc[i+2].value
         df = df.append({
             "train_loss": train_loss,
             "val_loss": val_loss,
-            # "kld_loss": kld_loss
+            "kld_loss": kld_loss
         }, ignore_index=True)
     print(df)
     df.to_pickle(f"{save_path}/{exp_name}.pkl")
 
 
 if __name__ == "__main__":
-    exp_name = "T1C_BC_24_LD_1024_MD_16_BS_2_TS_1500_LR_1e-05_ME_600_BETA_0001_1642258438"
+    exp_name = "VAE_T1C_BC_24_LD_2_MD_16_BS_2_TS_1500_LR_3e-05_ME_600_BETA_0001_1641834797"
+    # "VAE_T1C_BC_24_LD_4_MD_16_BS_2_TS_1500_LR_3e-05_ME_600_BETA_0001_1641900178"
+    # "VAE_T1C_BC_24_LD_8_MD_16_BS_2_TS_1500_LR_3e-05_ME_600_BETA_0001_1641830476"
+    # "VAE_T1C_BC_24_LD_32_MD_16_BS_2_TS_1500_LR_3e-05_ME_600_BETA_0001_1641806824"
+    # "VAE_T1C_BC_24_LD_128_MD_16_BS_2_TS_1500_LR_3e-05_ME_600_BETA_0001_1641806733"
     # "VAE_T1C_BC_24_LD_1024_MD_16_BS_2_TS_1500_LR_3e-05_ME_600_BETA_0001_1641757232"
     # "VAE_T1C_BC_24_LD_512_MD_16_BS_2_TS_1500_LR_3e-05_ME_600_BETA_0001_1641550824"
 
     # AE logs
+    # "FLAIR_BC_24_LD_1024_MD_16_BS_2_TS_1500_LR_1e-05_ME_300_BETA_0001_1642493260"
+    # "T1C_BC_24_LD_1024_MD_16_BS_2_TS_1500_LR_1e-05_ME_600_BETA_0001_1642258438"
     # "T1C_BC_24_LD_128_MD_16_BS_2_TS_1500_LR_1e-05_ME_120_BETA_0001_1641399246"
     # "T1C_BC_24_LD_512_MD_16_BS_2_TS_1500_LR_1e-05_ME_120_BETA_0001_1641390171"
     # "T1C_BC_24_LD_32_MD_16_BS_2_TS_1500_LR_1e-05_ME_120_BETA_0001_1641399376"
-    # "BC_24_LD_1024_MD_16_BS_2_TS_1500_LR_1e-05_ME_120_BETA_0001_1640617079"
     # "BC_24_LD_2048_MD_16_BS_2_TS_1500_LR_1e-05_ME_120_1638352499"
     # "BC_24_LD_4096_MD_16_BS_2_TS_1500_LR_1e-05_ME_120_1636735907"
     save_train_loss_as_pkl(exp_name)
