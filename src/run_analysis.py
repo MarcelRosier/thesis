6af6b837b778a10b0fd5994d463106727fa0dc90
Marcel Rosier <marcel.rosier@gmx.de>
2022-02-04 17:38:56 +0100

    - new t1c top15

--- src/run_analysis.py
+++ src/run_analysis.py
@@ -2,7 +2,7 @@ from baseline import analysis
 from autoencoder import recon_dice_analysis
 from utils import DSValueType
 analysis.compare_best_match_for_enc(
-    value_type=DSValueType.FLAIR, is_ae=False, is_1024=False, is_20k=True)
+    value_type=DSValueType.T1C, is_ae=False, is_1024=False, is_20k=True)
 # analysis.compare_best_match_for_enc(value_type=DSValueType.T1C)
 # recon_dice_analysis.compute_recon_dice_scores(is_t1c=True, cuda_id=6)
 # recon_dice_analysis.compare_custom_monai_ranking()
