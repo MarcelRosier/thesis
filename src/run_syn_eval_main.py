c6224016a671d2711a8381a1840bf377cda4d9e1
Marcel Rosier <marcel.rosier@gmx.de>
2022-02-17 11:11:44 +0100

    - adding second syn eval set

--- src/run_syn_eval_main.py
+++ src/run_syn_eval_main.py
@@ -1,9 +1,10 @@
 from constants import (ENV, SYN_TUMOR_PATH_TEMPLATE)
-from syn_eval import baseline, test, query
+from syn_eval import baseline
 
 SYN_TUMOR_PATH_TEMPLATE = SYN_TUMOR_PATH_TEMPLATE[ENV]
 
 STARTING_ID = 75000
+STARTING_ID_VAL = 80000
 
 
 def run_baseline(downsample_to: int = None):
@@ -12,9 +13,14 @@ def run_baseline(downsample_to: int = None):
         baseline.run(input_tumor_id=cur_id, downsample_to=downsample_to)
 
 
-# run_baseline(downsample_to=None)
+def run_baseline_validation(downsample_to: int = None):
+    for i in range(62):
+        cur_id = STARTING_ID_VAL + i
+        baseline.run(input_tumor_id=cur_id,
+                     downsample_to=downsample_to, validation=True)
+
+
+run_baseline_validation(downsample_to=None)
 # run_baseline(downsample_to=64)
 # run_baseline(downsample_to=32)
 # test.bm_presence()
-
-query.run(processes=32)
