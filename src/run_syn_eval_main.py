88f301f62b6239aa93692d5da5f5d8407a8cc50e
Marcel Rosier <marcel.rosier@gmx.de>
2022-02-20 12:35:55 +0100

    - first batch of 1k syn eval

--- src/run_syn_eval_main.py
+++ src/run_syn_eval_main.py
@@ -5,6 +5,7 @@ SYN_TUMOR_PATH_TEMPLATE = SYN_TUMOR_PATH_TEMPLATE[ENV]
 
 STARTING_ID = 75000
 STARTING_ID_VAL = 80000
+STARTING_LARGE_ID = 90000
 
 
 def run_baseline(downsample_to: int = None):
@@ -20,7 +21,15 @@ def run_baseline_validation(downsample_to: int = None):
                      downsample_to=downsample_to, validation=True)
 
 
-real_query_dc.run(processes=32)
+def run_large_syn_eval(downsample_to: int = None):
+    for i in range(1000):
+        cur_id = STARTING_LARGE_ID + i
+        baseline.run(input_tumor_id=cur_id,
+                     downsample_to=downsample_to)
+
+
+run_large_syn_eval()
+# real_query_dc.run(processes=32)
 # helper.run(val=True)
 # run_baseline_validation(downsample_to=None)
 # run_baseline(downsample_to=64)
